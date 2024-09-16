d0ed6f7f64b821c34a213f0b54f435bf84ff3640
Tinat1ni <tinatingagnidze121@gmail.com>
2024-09-16 18:56:06 +0400

    update main.py and data.json

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 import asyncio
 import aiohttp
 import json
+import time
 
 
-lock = asyncio.Lock()
-
-async def get_post(post_id, file):
-     url = f'https://jsonplaceholder.typicode.com/posts/{post_id}'
-     async with aiohttp.ClientSession() as session:
-          async with session.get(url, ssl = False) as response:
-               data = await response.json()
-               async with lock:
-                    with open(file, 'a') as f:
-                         if f.tell() == 0:
-                              f.write('[')
-                         else:
-                              f.write(', ')
-                         json.dump(data, f, indent = 4)
+async def get_post(post_id):
+    url = f'https://jsonplaceholder.typicode.com/posts/{post_id}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, ssl=False) as response:
+            return await response.json()
 
 
 async def main():
-     file = 'data.json'
-     tasks = []
-     post_ids = range(1,78)
+    file = 'data.json'
+    post_ids = range(1, 78)
 
-     with open(file, 'w') as f:
-          f.write('')
+    tasks = [asyncio.create_task(get_post(post_id)) for post_id in post_ids]
+    results = await asyncio.gather(*tasks)
 
-     for post_id in post_ids:
-          tasks.append(asyncio.create_task(get_post(post_id, file)))
-     await asyncio.gather(*tasks)
-
-     with open(file, 'a') as f:
-          f.write(']')
-
+    with open(file, 'w') as f:
+        json.dump(results, f, indent=4)
 
 
 if __name__ == '__main__':
-     asyncio.run(main())
+    start = time.time()
+    asyncio.run(main())
+    end = time.time()
+    print(end - start)
+
